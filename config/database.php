3fbf1aa9e1c52add8a3fb9f3e5741b8f9b38fcf0
Gavin Gassmann <gassmann.gavin@gmail.com>
2016-02-11 03:51:55 -0600

    Use ENV variables.
    Signed-off-by: Gavin Gassmann <gassmann.gavin@gmail.com>

--- config/database.php
+++ config/database.php
@@ -55,9 +55,9 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'database'  => 'db',
-            'username'  => 'root',
-            'password'  => 'Gigimoi00',
+            'database'  => env('DB_DATABASE', 'db'),
+            'username'  => env('DB_USERNAME', 'root'),
+            'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
